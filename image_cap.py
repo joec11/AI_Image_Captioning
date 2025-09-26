d0158e6250ddec08e5d8174944a021d18e1f091e
joec11 <joseph.candela0@gmail.com>
2025-09-25 20:49:21 -0400

    Generating image captions with the BLIP model.

---
+++ image_cap.py
@@ -0,0 +1,26 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Load the image
+img_path = "image_captioning_1.png"
+
+# Convert it into an RGB format 
+image = Image.open(img_path).convert('RGB')
+
+# A question is not needed for image captioning
+text = "the image of"
+inputs = processor(images = image, text = text, return_tensors = "pt")
+
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length = 50)
+
+# Decode the generated tokens to text
+caption = processor.decode(outputs[0], skip_special_tokens = True)
+
+# Print the caption
+print(caption)
